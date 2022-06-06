553c970500083899580de1133adca0ef494a0f03
mansamusah254 <mansamusah830@gmail.com>
2022-06-07 00:02:48 +0300

    failed to parse data between activities

--- app/src/main/java/com/cookingapp/pika/MainActivity.java
+++ app/src/main/java/com/cookingapp/pika/MainActivity.java
@@ -1,35 +1,44 @@
 package com.cookingapp.pika;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button mletsCookButton;
     private Button mMoreButton;
     private Button mAddRecipeButton;
+    private EditText mfoodName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mfoodName = (EditText) findViewById(R.id.foodName);
+        mAddRecipeButton = (Button) findViewById(R.id.AddRecipeButton);
+        mAddRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String foodName = mfoodName.getText().toString();
+                Intent intent = new Intent(MainActivity.this,RecipeActivity.class);
+                intent.putExtra("foodName", foodName);
+                startActivity(intent);
+            }
+        });
+
+
         mletsCookButton = (Button) findViewById(R.id.letsCookButton);
         mletsCookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LayoutActivity();
+                Intent intent = new Intent(MainActivity.this,LayoutActivity.class);
+                startActivity(intent);
             }
         });
     }
-    public void LayoutActivity(){
-        Intent intent = new Intent(this, LayoutActivity.class);
-        startActivity(intent);
-    }
-
 }
--- app/src/main/java/com/cookingapp/pika/RecipeActivity.java
+++ app/src/main/java/com/cookingapp/pika/RecipeActivity.java
@@ -2,16 +2,31 @@ package com.cookingapp.pika;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecipeActivity extends AppCompatActivity {
 private Button mAddRecipeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
-    }
-}
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("foodName");
+
+//        mAddRecipeButton = (Button) findViewById(R.id.AddRecipeButton);
+//        mAddRecipeButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                RecipeActivity();
+//            }
+//        });
+//    }
+//    public void RecipeActivity(){
+//        Intent intent = new Intent(this, RecipeActivity.class);
+//        startActivity(intent);
+//    }
+}}
