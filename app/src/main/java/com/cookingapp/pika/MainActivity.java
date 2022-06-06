5b5a41194e114be829c710b881fd7f93cc714c02
mansamusah254 <mansamusah830@gmail.com>
2022-06-06 18:06:13 +0300

    new activity

--- app/src/main/java/com/cookingapp/pika/MainActivity.java
+++ app/src/main/java/com/cookingapp/pika/MainActivity.java
@@ -2,13 +2,34 @@ package com.cookingapp.pika;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mletsCookButton;
+    private EditText mFoodNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFoodNameEditText = (EditText)findViewById(R.id.FoodName)
+        mletsCookButton = (Button)findViewById(R.id.letsCookButton);
+         mletsCookButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                 startActivity(intent);
+//                 intent.putExtra("Food Name", food name);
+                 startActivity(intent);
+             }
+         });
     }
+
+
 }
