8b67dde1b81305ae587d5895a049574e1068ec61
mansamusah254 <mansamusah830@gmail.com>
2022-06-07 13:41:34 +0300

    Butter knife implementation

--- app/src/main/java/com/cookingapp/pika/LayoutActivity.java
+++ app/src/main/java/com/cookingapp/pika/LayoutActivity.java
@@ -1,18 +1,35 @@
 package com.cookingapp.pika;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LayoutActivity extends AppCompatActivity {
     private Button mMoreButton;
+    private Button mAddRecipeButton;
+    private EditText mfoodName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout);
+
+        mfoodName = (EditText) findViewById(R.id.foodName);
+        mAddRecipeButton = (Button) findViewById(R.id.AddRecipeButton);
+        mAddRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String foodName = mfoodName.getText().toString();
+                Intent intent = new Intent(LayoutActivity.this,RecipeActivity.class);
+                intent.putExtra("foodName", foodName);
+                startActivity(intent);
+            }
+        });
+
         mMoreButton = (Button) findViewById(R.id.MoreButton);
         mMoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/cookingapp/pika/MainActivity.java
+++ app/src/main/java/com/cookingapp/pika/MainActivity.java
@@ -4,41 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private Button mletsCookButton;
-    private Button mMoreButton;
-    private Button mAddRecipeButton;
-    private EditText mfoodName;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.letsCookButton) Button mLetsCookButton;
+    @BindView(R.id.Title) TextView mTitle;
+    @BindView(R.id.Welcome) TextView mWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mfoodName = (EditText) findViewById(R.id.foodName);
-        mAddRecipeButton = (Button) findViewById(R.id.AddRecipeButton);
-        mAddRecipeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String foodName = mfoodName.getText().toString();
-                Intent intent = new Intent(MainActivity.this,RecipeActivity.class);
-                intent.putExtra("foodName", foodName);
-                startActivity(intent);
-            }
-        });
-
-
-        mletsCookButton = (Button) findViewById(R.id.letsCookButton);
-        mletsCookButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,LayoutActivity.class);
-                startActivity(intent);
-            }
-        });
+        mLetsCookButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == mLetsCookButton) {
+            Intent intent = new Intent(MainActivity.this, LayoutActivity.class);
+            startActivity(intent);
+        }
     }
 }
